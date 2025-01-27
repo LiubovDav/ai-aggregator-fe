6f1f7ae310a3afe1eac0a3dbe53c804b3d4ecad4
Liubov Dav <lubasha0512@gmail.com>
2025-01-27 10:20:17 -0800

    Created app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" crc="/assets/logo.png" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+
+      </section>
+  </main>
+      `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ai-aggregator-fe';
+  title = 'AI Aggregator';
 }
+
+
