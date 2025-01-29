95f316c5a9ea1292a6b39f6831edd9c612e38180
Liubov Dav <lubasha0512@gmail.com>
2025-01-29 12:34:44 -0800

    Refactor AppComponent to use external template and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
-  // templateUrl: './app.component.html',
-  template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" crc="/assets/logo.png" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-
-      </section>
-  </main>
-      `,
-  styleUrl: './app.component.css',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AI Aggregator';
