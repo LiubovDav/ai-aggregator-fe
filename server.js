9ff0f689e9661f124c1b5680cc9410df79360fd9
Liubov Dav <lubasha0512@gmail.com>
2025-01-29 14:13:51 -0800

    Fixed static file path in server.js for correct deployment

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<ai-aggregator-fe>'));
+app.use(express.static(__dirname + '/dist/ai-aggregator-fe'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/<ai-aggregator-fe>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ai-aggregator-fe/index.html'));
 });
 app.listen(process.env.PORT || 4200);
