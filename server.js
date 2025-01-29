05ca5807e12c998940b737bdb51a628ffe46862f
Liubov Dav <lubasha0512@gmail.com>
2025-01-29 14:03:44 -0800

    Changed server port to 4200 for local development

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static(__dirname + '/dist/<ai-aggregator-fe>'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/<ai-aggregator-fe>/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
