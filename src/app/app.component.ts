eb24af97b76bb39b7db9e7a57154490ecd3008d2
Liubov Dav <lubasha0512@gmail.com>
2025-01-31 08:34:19 -0800

    Added ChatModel and UserCreate components to app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
 import { MenubarComponent } from "./menubar/menubar.component";
 import { UserListComponent } from "./user-list/user-list.component";
+import { UserCreateComponent } from "./user-create/user-create.component";
+import { ChatModelComponent } from "./chat-model/chat-model.component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, MenubarComponent, UserListComponent],
+  imports: [RouterOutlet, MenubarComponent, MatDividerModule, UserListComponent, UserCreateComponent, ChatModelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
