ec2a079bcfe99cb5e6b6517832e7e7761a776c1a
Liubov Dav <lubasha0512@gmail.com>
2025-02-02 00:57:30 -0800

    Added Login and Logout components with routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,13 @@ import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { ChatModelComponent } from './chat-model/chat-model.component';
 import { ImageModelComponent } from './image-model/image-model.component';
-import { UserCreateComponent } from './user-create/user-create.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ChatModelComponent
-  },
-  {
-    path: 'login',
-    component: ChatModelComponent // todo: create new component
+    component: LoginComponent
   },
   {
     path: 'chat-model',
@@ -27,6 +24,6 @@ export const routes: Routes = [
   },
   {
     path: 'logout',
-    component: UserCreateComponent // todo: create new component
+    component: LogoutComponent
   }
 ];
